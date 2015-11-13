d2af9525c47871cae093a0b51f14869e2a3604ea
abdesslem <amriabdesslem@gmail.com>
2015-11-13 12:53:06 +0100

    add the ip address to tornado web server

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from tornado.ioloop import IOLoop
 from womanctf import app
 
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
+http_server.listen(5000,'0.0.0.0')
 IOLoop.instance().start()
